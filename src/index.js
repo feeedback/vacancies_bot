141d36c6441dc6c4b7f3cbd417b6635c488d0502
A. Rzhanitsyn <risdeep@gmail.com>
2021-04-18 02:28:59 +0500

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import vacancyExcludeTags from '../data/exclude_tags.js';
-import vacancyExcludeWordsInDesc from '../data/exclude_words_desc.js';
+import vacancyExcludeTagsMy from '../data/exclude_tags.js';
+import vacancyExcludeWordsInDescMy from '../data/exclude_words_desc.js';
 import {
   getVacancyByFilterFromRssHabrCareer,
   parseFilterFormatVacancies,
@@ -20,7 +20,12 @@ import {
 // const URL_RSS =
 //   'https://career.habr.com/vacancies/rss?currency=RUR&divisions[]=apps&divisions[]=software&divisions[]=backend&divisions[]=frontend&salary=40000&skills[]=264&sort=date&type=all&with_salary=1';
 
-const getRss = async (url, day = 2) => {
+const getRss = async (
+  url,
+  day = 2,
+  vacancyExcludeTags = vacancyExcludeTagsMy,
+  vacancyExcludeWordsInDesc = vacancyExcludeWordsInDescMy
+) => {
   const vacanciesRaw = await getVacancyByFilterFromRssHabrCareer(url, day);
   const { vacanciesFiltered, vacancies } = await parseFilterFormatVacancies(
     vacanciesRaw,
--- src/telegram.js
+++ src/telegram.js
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import 'dotenv/config.js';
-import { Telegraf, Markup } from 'telegraf';
+import { Telegraf } from 'telegraf';
 import { URL } from 'url';
 import _ from 'lodash';
+import { markdownEscapes } from 'markdown-escapes';
 import getRss from './index.js';
-
+import vacancyExcludeTagsMy from '../data/exclude_tags.js';
+// import vacancyExcludeWordsInDescMy from '../data/exclude_words_desc.js';
+const markdownRegexp = new RegExp(`([${markdownEscapes.join('')}])`);
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API);
 // bot.start((ctx) => ctx.reply('Welcome'));
 // bot.help((ctx) => ctx.reply('Send me a sticker'));
@@ -18,9 +22,9 @@ const mapUserIdToState = {
   413777946: {
     rss: MY_URL_RSS,
     rssMessageID: null,
-    pollOptions: null,
-    selectedPollOptionsIndex: null,
-    excludeTags: [],
+    pollOptions: null, // []
+    selectedPollOptionsIndex: null, // []
+    excludeTags: [...vacancyExcludeTagsMy],
   },
 };
 
@@ -35,7 +39,7 @@ const setRss = async (ctx, rss) => {
 
   console.log(rss, isValidURL);
   if (!isValidURL) {
-    ctx.reply('invalid RSS url, need starts with "https://career.habr.com/vacancies/rss"');
+    ctx.reply('Invalid RSS url, need starts with "https://career.habr.com/vacancies/rss"', { disable_web_page_preview: true });
     return;
   }
 
@@ -47,23 +51,53 @@ const setRss = async (ctx, rss) => {
   mapUserIdToState[userId].rss = rss;
   await ctx.reply('Saved your RSS successful!');
   console.log('user id', userId, 'rss saved', rss);
+};
 
-  const { topTagsByCount, topTagsByCountByFiltered } = await getRss(rss, 2);
+const setExcludeTags = async (ctx, isSaveOld = false) => {
+  const userId = ctx.update.message.from.id;
+  if (!mapUserIdToState[userId]) {
+    mapUserIdToState[userId] = {};
+  }
+  if (!mapUserIdToState[userId].rss) {
+    ctx.reply('Для начала установите RSS ссылку, командой */rss*', { parse_mode: 'Markdown' });
+    return;
+  }
+
+  if (!mapUserIdToState[userId]?.excludeTags) {
+    mapUserIdToState[userId].excludeTags = [];
+  }
+  if (!isSaveOld) {
+    mapUserIdToState[userId].excludeTags = [];
+  }
+
+  const { topTagsByCount, topTagsByCountByFiltered } = await getRss(
+    mapUserIdToState[userId].rss,
+    10
+  );
   // await ctx.poll([topTagsByCount]);
   // await ctx.telegram.sendPoll(ctx.chatId: string | number, question: string, options: topTagsByCount, extra ?: ExtraPoll)
-  const topTags = Object.entries(topTagsByCount)
+  const topTags = Object.entries(isSaveOld ? topTagsByCountByFiltered : topTagsByCount)
     .filter(([, count]) => count >= 2)
     .map(([tag]) => tag);
-  const pollOptions = topTags.slice(0, 10);
 
-  mapUserIdToState[userId].pollOptions = pollOptions;
-  const poll = await ctx.telegram.sendPoll(
-    ctx.chat.id,
-    'Выберите теги, вакансии с которыми исключить из выдачи',
-    pollOptions,
-    { allows_multiple_answers: true, is_anonymous: false }
-  );
-  console.log(poll);
+  const chunkedTags = _.chunk(topTags, 10);
+
+  if (!mapUserIdToState[userId].pollOptions) {
+    mapUserIdToState[userId].pollOptions = {};
+  }
+  for (const pollOptions of chunkedTags) {
+    if (pollOptions.length < 2) {
+      break;
+    }
+    const { poll } = await ctx.telegram.sendPoll(
+      ctx.chat.id,
+      'Выберите теги, вакансии с которыми исключить из выдачи',
+      pollOptions,
+      { allows_multiple_answers: true, is_anonymous: false }
+    );
+
+    mapUserIdToState[userId].pollOptions[poll.id] = pollOptions;
+  }
 };
 
 bot.start((ctx) => ctx.replyWithDice());
@@ -72,10 +106,19 @@ bot.settings(async (ctx) => {
     { command: '/rss', description: 'set rss link' },
     { command: '/get', description: 'get last day vacancies' },
     { command: '/ex_tags', description: 'add tags, vacancies with which will be excluded' },
+    { command: '/get_tags', description: 'get list your excluded tags' },
     {
-      command: '/ex_words',
-      description: 'add words, vacancies with which in description will be excluded',
+      command: '/ex_tags_add',
+      description: 'add new tags with save old, vacancies with which will be excluded',
     },
+    // {
+    //   command: '/ex_words',
+    //   description: 'add words, vacancies with which in description will be excluded',
+    // },
+    // {
+    //   command: '/ex_words_add',
+    //   description: 'add words, vacancies with which in description will be excluded',
+    // },
   ]);
 });
 
@@ -95,7 +138,7 @@ bot.use(async (ctx, next) => {
 bot.command('rss', async (ctx) => {
   if (ctx.update.message.text.trim() === '/rss') {
     const message = await ctx.reply(
-      'please copy/paste RSS link from Habr.Career with yours filters https://career.habr.com/vacancies',
+      'please copy/paste RSS link from Habr. Career with yours filters https://career.habr.com/vacancies',
       { disable_web_page_preview: true }
     );
     const userId = ctx.update.message.from.id;
@@ -107,12 +150,44 @@ bot.command('rss', async (ctx) => {
   await setRss(ctx, rss);
 });
 
+bot.command('ex_tags', async (ctx) => {
+  await setExcludeTags(ctx);
+});
+bot.command('get_tags', async (ctx) => {
+  const userId = ctx.update.message.from.id;
+  if (!mapUserIdToState[userId]?.excludeTags) {
+    ctx.reply('Для начала установите RSS ссылку */rss*', {
+      parse_mode: 'Markdown',
+    });
+    return;
+  }
+  if (!mapUserIdToState[userId].excludeTags.length === 0) {
+    ctx.reply('Ваш список исключаемых тегов пуст. Вы можете добавить их командой */ex_tags*', {
+      parse_mode: 'Markdown',
+    });
+    return;
+  }
+  const tagsStr = mapUserIdToState[userId].excludeTags
+    // eslint-disable-next-line no-useless-escape
+    .map((tag) => `  \`#${tag.replace(markdownRegexp, '\$1')}\``)
+    .join('\n');
+
+  ctx.reply(
+    `*Ваши исключаемые теги:*\n${tagsStr}\n\nВы можете добавить в список ещё */ex_tags_add* (или задать заново */ex_tags*)`,
+    { disable_web_page_preview: true, parse_mode: 'Markdown' }
+  );
+});
+
+bot.command('ex_tags_add', async (ctx) => {
+  await setExcludeTags(ctx, true);
+});
+
 bot.command('get', async (ctx) => {
   const userId = ctx.update.message.from.id;
   const rss = mapUserIdToState[userId]?.rss;
 
   if (!rss) {
-    ctx.reply('RSS not found! Please add that with /rss [link]');
+    ctx.reply('RSS not found! Please add that with */rss* [link]', { parse_mode: 'Markdown' });
     console.log('user id', userId, 'not found rss');
     return;
   }
@@ -128,7 +203,7 @@ bot.command('get', async (ctx) => {
   ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing');
 
   try {
-    const { stringVacancies } = await getRss(rss, day);
+    const { stringVacancies } = await getRss(rss, day, mapUserIdToState[userId].excludeTags);
     console.log('вакансии получены', stringVacancies.length);
     const message = stringVacancies.join('\n\n');
 
@@ -166,17 +241,22 @@ bot.on('text', async (ctx) => {
 
 bot.on('poll_answer', (ctx) => {
   const { poll_answer: poll } = ctx.update;
-
   if (poll.user.is_bot) {
     return;
   }
-
+  // ctx.update.
   // const userId = poll.user.id;
+  if (!mapUserIdToState[poll.user.id]?.pollOptions) {
+    return;
+  }
   const excludeTags = poll.option_ids.map(
-    (index) => mapUserIdToState[poll.user.id].pollOptions[index]
+    (index) => mapUserIdToState[poll.user.id].pollOptions[poll.poll_id][index]
   );
-  mapUserIdToState[poll.user.id].excludeTags = excludeTags;
-  console.log({ excludeTags });
+  mapUserIdToState[poll.user.id].excludeTags = [
+    ...mapUserIdToState[poll.user.id].excludeTags,
+    ...excludeTags,
+  ];
+  console.log(mapUserIdToState[poll.user.id].excludeTags);
   // setTimeout(() => {
   //   ище.stopPoll(ctx.update.poll_answer.poll_id).then((poll) => console.log(poll));
   // }, 1000);
