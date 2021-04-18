6c272ac5e1bae68c0167c33cc1d3c1edbb945952
A. Rzhanitsyn <risdeep@gmail.com>
2021-04-18 17:15:11 +0500

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,27 +3,23 @@ module.exports = {
   env: {
     node: true,
     es2020: true,
-    jest: true
+    jest: true,
   },
   extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
   plugins: ['jest', 'prettier'],
   globals: {},
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   parser: 'babel-eslint',
   rules: {
     'import/extensions': 0,
-    'no-underscore-dangle': [2, { 'allow': ['__filename', '__dirname'] }],
+    'no-underscore-dangle': [2, { allow: ['__filename', '__dirname'] }],
     'no-restricted-syntax': ['off', 'ForOfStatement'],
     'no-await-in-loop': 'off',
     'dot-notation': ['off'],
     'no-console': 'off',
-    'prettier/prettier': [
-      'error',
-      { endOfLine: 'auto', printWidth: 100 }
-    ],
-    'prettier/prettier': 'error',
-  }
-}
+    'prettier/prettier': ['error', { endOfLine: 'auto', printWidth: 100 }],
+  },
+};
