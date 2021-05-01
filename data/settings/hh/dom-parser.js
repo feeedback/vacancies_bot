8009df9014ff86aa5b1ec02064ea3731c2286ad1
A. Rzhanitsyn <risdeep@gmail.com>
2021-05-02 01:12:41 +0500

    add

--- data/settings/hh/dom-parser.js
+++ data/settings/hh/dom-parser.js
@@ -1,27 +1,56 @@
 const BASE_VACANCY_LINK = 'https://hh.ru/vacancy';
-
-const vacanciesEl = [...document.querySelectorAll('.vacancy-serp-item')];
-
-const tasks = vacancy.querySelector('[data-qa="vacancy-serp__vacancy_snippet_responsibility"]')
-  .textContent;
-const skills = vacancy.querySelector('[data-qa="vacancy-serp__vacancy_snippet_requirement"]')
-  .textContent;
-const title = vacancy.querySelector('[data-qa="vacancy-serp__vacancy-title"]').textContent;
-const id = vacancy
-  .querySelector('[data-qa="vacancy-serp__vacancy-title"]')
-  .split('?')[0]
-  .split('vacancy/')[1];
-const link = `${BASE_VACANCY_LINK}/${id}`;
-
-const salaryStr = vacancy.querySelector('[data-qa="vacancy-serp__vacancy-compensation"]')
-  .textContent;
-const company = vacancy.querySelector('[data-qa="vacancy-serp__vacancy-employer"]').textContent;
-const address = vacancy.querySelector('[data-qa="vacancy-serp__vacancy-address"]').textContent;
-const dateMonthDay = vacancy.querySelector(
-  '[data-qa="vacancy-serp__vacancy-date"] .vacancy-serp-item__publication-date_short'
-).textContent;
+const REGEX_SALARY = /(?:(?:^(?:от)\s*(?:(\d[\s\d]*\d)+))|(?:^(?:до)\s*(?:(\d[\s\d]*\d)+))|(?:^(?:(\d[\s\d]*\d)+)-(?:(\d[\s\d]*\d)+)))(?: (.+)$)/gi;
 const mapCurrencyStrToCode = {
   'USD': 'USD',
   'руб.': 'RUB',
 };
-const REGEX_SALARY = /(?:(?:^(?:от)\s*(?:(\d[\s\d]*\d)+))|(?:^(?:до)\s*(?:(\d[\s\d]*\d)+))|(?:^(?:(\d[\s\d]*\d)+)-(?:(\d[\s\d]*\d)+)))(?: (.+)$)/gi;
+
+const vacanciesEl = [...document.querySelectorAll('.vacancy-serp-item')];
+const getChildTextByDataAttr = (el, str, addStr = '', isText = true) => {
+  const child = el.querySelector(`[data-qa="${str}"] ${addStr}`);
+  return isText ? child.textContent : child;
+};
+
+const vacanciesData = vacanciesEl.map((vacancy) => {
+  const getElByAttr = (attr, selector) => getChildTextByDataAttr(vacancy, attr, selector);
+
+  const tasks = getElByAttr('vacancy-serp__vacancy_snippet_responsibility');
+  const skills = getElByAttr('vacancy-serp__vacancy_snippet_requirement');
+  const title = getElByAttr('vacancy-serp__vacancy-title');
+
+  const id = getElByAttr('vacancy-serp__vacancy-title', '', false)
+    .href.split('?')[0]
+    .split('vacancy/')[1];
+  const link = `${BASE_VACANCY_LINK}/${id}`;
+
+  const salaryStr = getElByAttr('vacancy-serp__vacancy-compensation').trim();
+  const [, rawMin, rawMax, rawFrom, rawTo, rawCurrencySymbol] = salaryStr.match(REGEX_SALARY);
+
+  const salary = {
+    rawMin,
+    rawMax,
+    rawFrom,
+    rawTo,
+    rawCurrencySymbol: mapCurrencyStrToCode[rawCurrencySymbol],
+  };
+  const company = getElByAttr('vacancy-serp__vacancy-employer');
+  const address = getElByAttr('vacancy-serp__vacancy-address');
+
+  const dateMonthDay = getElByAttr(
+    'vacancy-serp__vacancy-date',
+    '.vacancy-serp-item__publication-date_short'
+  );
+  return {
+    tasks,
+    skills,
+    title,
+    id,
+    link,
+    salary,
+    company,
+    address,
+    dateMonthDay,
+  };
+});
+
+console.log(vacanciesData);
--- data/settings/hh/hh.js
+++ data/settings/hh/hh.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { excludeWordTitle, excludeWordDesc, includeWordDesc } from './hh_words.js';
 
 const syntax = {
@@ -14,21 +15,25 @@ const syntax = {
 const exTitle = syntax.BY_TITLE(syntax.EXCLUDE(syntax.OR(...excludeWordTitle)));
 const exDesc = syntax.BY_DESC(syntax.EXCLUDE(syntax.OR(...excludeWordDesc)));
 const inc = syntax.BY_ALL(syntax.INCLUDE(syntax.OR(...includeWordDesc)));
+const searchText = syntax.ALL(inc, exTitle, exDesc);
 
-console.log(syntax.ALL(inc, exTitle, exDesc));
-// const BASE_URL = 'https://hh.ru/search/vacancy?';
-// // qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'repeat' })
-// const filterVacanciesSearch = {
-//   L_is_autosearch: true,
-//   date_from: '30.04.2021 10:51:11',
+console.log(searchText);
 
-//   salary: 50000,
-//   schedule: ['remote'],
-//   employment: ['full', 'part', 'probation'],
-//   experience: ['between1And3'],
-//   order_by: 'publication_time',
-//   no_magic: true, // ???
-//   items_on_page: 100,
-//   only_with_salary: true,
-//   text: searchText,
-// };
+const BASE_URL = 'https://hh.ru/search/vacancy?';
+// qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'repeat' })
+
+const filterVacanciesSearch = {
+  L_is_autosearch: true,
+  // date_from: '30.04.2021 10:51:11',
+  date_from: dayjs().subtract('5', 'minute').format('DD.MM.YYYY HH:mm:ss'), // last request time
+
+  salary: 40000,
+  schedule: ['remote'],
+  employment: ['full', 'part', 'probation'],
+  experience: ['between1And3'],
+  order_by: 'publication_time',
+  no_magic: true, // ???
+  items_on_page: 100,
+  only_with_salary: true,
+  text: searchText,
+};
